337686a34fe2142b99bd8e0546320aaa1ecee4b0
Akhil Krishnan <akhil@dataguardnxt.com>
2022-11-07 18:24:03 +0530

    first part of bloc tutorial

---
+++ lib/counter_bloc/counter_state.dart
@@ -0,0 +1,11 @@
+part of 'counter_bloc.dart';
+
+
+class CounterState {
+  int counter;
+  CounterState({required this.counter});
+}
+
+class CounterInitial extends CounterState {
+  CounterInitial():super(counter: 0);
+}
